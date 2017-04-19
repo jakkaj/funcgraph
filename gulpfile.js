f1e65b90f15433ecf7f873147f556aac743c3189
Jordan Knight <jakkaj@gmail.com>
2017-04-19 16:16:04 +1000

    Gulp work on clean release builds

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,48 @@
 var gulp = require('gulp');
 var typescript = require('gulp-tsc');
 var fs = require("fs");
-var dest = "SiteExtension/funcgraph";
+var dest = "build/debug/SiteExtension/funcgraph";
+var dest_rel = "build/release/SiteExtension/funcgraph";
+var del = require('del');
 
 gulp.task('build', function() {
-    gulp.start("compile");
-    gulp.start("copyFiles");
+    return [gulp.start("compile"),
+    gulp.src(["package.json", 'src/applicationHost.xdt', 'src/web.config'])
+        .pipe(gulp.dest(dest))
+    ];
 });
 
+gulp.task('build_release', function() {
+    return [gulp.start("compile_release"),    
+    gulp.src(["package.json", 'src/applicationHost.xdt', 'src/web.config'])
+        .pipe(gulp.dest(dest_rel))
+    ];
+});
 
+gulp.task('clean:release', function () {
+  return del([
+    dest_rel + '/**/*'
+  ]);
+});
+
+gulp.task('clean:dev', function () {
+  return del([
+    dest + '/**/*'
+  ]);
+});
  
 gulp.task('compile', function(){
 var tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
   gulp.src(['src/**/*.ts'])
     .pipe(typescript(tsconfig.compilerOptions))
-    .pipe(gulp.dest(dest))
-});
-
-gulp.task('tsc', function() {
-    var tsResult = gulp.src("src/**/*.ts") // or tsProject.src() 
-        .pipe(sourcemaps.init())
-        .pipe(tsProject());
- 
-    return tsResult.js
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest(dest));
 });
 
-gulp.task('copyFiles', function(){
-    return gulp.src("package.json").pipe(gulp.dest(dest));
-})
+gulp.task('compile_release',["clean:release"], function(){
+
+
+var tsconfig = JSON.parse(fs.readFileSync('tsconfig_release.json', 'utf8'));
+  gulp.src(['src/**/*.ts'])
+    .pipe(typescript(tsconfig.compilerOptions))
+    .pipe(gulp.dest(dest_rel));
+});
