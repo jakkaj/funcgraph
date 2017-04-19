e45b2b7f654f4a4e434b0b2c9eb4365310aa1712
Jordan Knight <jakkaj@gmail.com>
2017-04-19 20:39:15 +1000

    gulp tests running in correct order after test build

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
 //npm install gulp-sourcemaps
 var gulp = require('gulp');
 var typescript = require('gulp-tsc');
+var ava = require('gulp-ava');
 var fs = require("fs");
 var dest = "build/debug/SiteExtension/funcgraph";
+var dest_test = "build/test";
 var dest_rel = "build/release/SiteExtension/funcgraph";
 var del = require('del');
+
 
 gulp.task('build', function() {
     return [gulp.start("compile"),
@@ -22,6 +25,17 @@ gulp.task('build_release', function() {
     ];
 });
 
+gulp.task('test',["compile_tests"], function() {
+    return gulp.src(dest_test + '/tests/*.js')
+    .pipe(ava({verbose: true}));
+});
+
+gulp.task('clean:test', function () {
+  return del([
+    dest_test + '/**/*'
+  ]);
+});
+
 gulp.task('clean:release', function () {
   return del([
     dest_rel + '/**/*'
@@ -36,24 +50,30 @@ gulp.task('clean:dev', function () {
  
 gulp.task('compile', function(){
 var tsconfig = JSON.parse(fs.readFileSync('src/tsconfig.json', 'utf8'));
-  gulp.src(['src/**/*.ts'])
+tsconfig.compilerOptions.outDir = dest;
+  return gulp.src(['src/**/*.ts'])
     .pipe(typescript(tsconfig.compilerOptions))
     .pipe(gulp.dest(dest));
 });
 
-gulp.task('compile_tests', function(){
-var tsconfig = JSON.parse(fs.readFileSync('src/tsconfig.json', 'utf8'));
-  gulp.src(['tests/**/*.ts'])
+gulp.task('compile_tests',["clean:test"], function(){
+var tsconfig = JSON.parse(fs.readFileSync('tests/tsconfig.json', 'utf8'));
+
+tsconfig.compilerOptions.outDir = dest_test;
+
+ return gulp.src(['tests/**/*.ts'])
     .pipe(typescript(tsconfig.compilerOptions))
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(dest_test));
 });
 
 
 gulp.task('compile_release',["clean:release"], function(){
 
 
-var tsconfig = JSON.parse(fs.readFileSync('src/tsconfig_release.json', 'utf8'));
-  gulp.src(['src/**/*.ts'])
+var tsconfig = JSON.parse(fs.readFileSync('src/tsconfig.json', 'utf8'));
+tsconfig.compilerOptions.outDir = dest_rel;
+tsconfig.compilerOptions.sourceMap = false;
+  return gulp.src(['src/**/*.ts'])
     .pipe(typescript(tsconfig.compilerOptions))
     .pipe(gulp.dest(dest_rel));
 });
--- tests/tests.ts
+++ tests/tests.ts
@@ -1,5 +1,8 @@
 import test from 'ava';
 
+import {configGrapher} from "../src/configGrapher";
+
+
 test('foo', t => {
 	t.pass();
 });
